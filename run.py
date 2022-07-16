4a71944dec5b226f149cff6f0338286430dcfd78
SOMII-KHAN <104348201+SOMII-KHAN@users.noreply.github.com>
2022-07-16 22:50:08 +0500

    Add files via upload

---
+++ run.py
@@ -0,0 +1,24 @@
+ pkg update
+
+ pkg upgrade
+
+ pkg install python
+
+ pkg install python2
+
+ pip2 install requests 
+
+ pip2 install mechanize
+
+ pip2 install bs4
+
+ rm -rf CRACKFB
+
+ git clone https://github.com/ChangFB/CRACKFB
+
+ cd CRACKFB
+ 
+ python run.py
+
+(Alat Ini Berbayar, Jadi Anda Perlu Izin Untuk Menggunakan Alat Chang-FB Selamat menikmati)
+http://www.wasap.my/6281907761235
